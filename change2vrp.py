a5d1c8aed5ae9217bcb5b8115086353cff6fced8
GOOEJUN <447080714@qq.com>
2020-11-27 23:59:09 +0800

    '改完了'

--- change2vrp.py
+++ change2vrp.py
@@ -1,15 +1,19 @@
-from test import nodeneedarray
-
-
 def change2vrp(Route,nodeneedarray,cap):
     fRoute = []
     sum = 0
-    for i in range(Route):
-        sum += nodeneedarray
+    for i in range(len(Route)):
+        j = Route[i]
+        sum += nodeneedarray[j].need
+        if sum > cap:
+            fRoute.append(0)
+            fRoute.append(j)
+            sum = 0
+        else:
+            fRoute.append(j)
 
     return fRoute
 
 class nodeneed:
-    def __init__(self):
-        node = 0
-        need = 0
+    def __init__(self,nood,need):
+        self.node = nood
+        self.need = need
